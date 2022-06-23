a6d041e852461efe7b7705c57e9af4d7d158f9c1
Enes Çetintaş <enesctnts@gmail.com>
2022-06-24 00:39:06 +0300

    Add project files.

---
+++ EfCore.DatabaseFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using EfCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace EfCore.DatabaseFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DbContexInitializer.Build();
+            using (var context = new AppDbContext(DbContexInitializer.OptionsBuilder.Options))
+            {
+               var products=  context.Products.ToListAsync().Result;
+                products.ForEach(p=>
+                Console.WriteLine($"{p.Name} {p.Price}") 
+                );
+            }
+            
+        }
+    }
+}
